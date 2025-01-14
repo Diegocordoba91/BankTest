a1b608b1648c92adeab7959fa309c169c2cc04db
Diego Cordoba <diegocordobaingeniero@gmail.com>
2025-01-14 12:23:37 -0500

    Integración de proyecto a Jenkins

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Compile Stage') {
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven_3_9_5', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn clean compile"
+                    }
+                }
+
+            }
+        }
+
+        stage('Test Stage') {
+            steps {
+                script {
+                    def mvnHome = tool name: 'apache-maven-3.9.9', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn clean test"
+                    }
+                }
+            }
+
+        }
+
+        stage('Cucumber Reports') {
+            steps {
+                cucumber buildStatus: "UNSTABLE",
+                        fileIncludePattern: "**/cucumber.json",
+                        jsonReportDirectory: 'target'
+            }
+        }
+
+    }
+
+}
